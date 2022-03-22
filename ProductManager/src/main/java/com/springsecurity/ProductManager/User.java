a003de5f069acefdd2249ee07bc6b01ba2f8b532
Iftekharmahmud <fuadiftekhar@gmail.com>
2022-03-22 12:19:35 +0600

    Springdatajpawithsecurity

---
+++ ProductManager/src/main/java/com/springsecurity/ProductManager/User.java
@@ -0,0 +1,68 @@
+package com.springsecurity.ProductManager;
+
+import javax.persistence.*;
+
+@Entity
+@Table(name = "user")
+public class User {
+
+    @Id
+    @Column(name = "id")
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+  /*  @SequenceGenerator(
+            name = "hibernate_sequence",
+            sequenceName ="hibernate_sequence",
+            allocationSize = 1
+    )
+    @GeneratedValue(
+            strategy = GenerationType.SEQUENCE,
+            generator = "hibernate_sequence"
+    )*/
+    private Long id;
+
+    private String username;
+    private String password;
+    private String role;
+    private boolean enabled;
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getUsername() {
+        return username;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public String getRole() {
+        return role;
+    }
+
+    public void setRole(String role) {
+        this.role = role;
+    }
+
+    public boolean isEnabled() {
+        return enabled;
+    }
+
+    public void setEnabled(boolean enabled) {
+        this.enabled = enabled;
+    }
+
+}
---
+++ ProductManager/src/main/java/com/springsecurity/ProductManager/UserDetailsServiceImpl.java
@@ -0,0 +1,28 @@
+package com.springsecurity.ProductManager;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+
+public class UserDetailsServiceImpl implements UserDetailsService {
+
+
+    /*Working with Spring Data Repositories
+The goal of Spring Data repository abstraction is to significantly reduce the amount of boilerplate
+ code required to implement data access layers for various persistence stores.*/
+    @Autowired
+    private UserRepository userRepository;
+
+    @Override
+    public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
+        User user = userRepository.getUserByUsername(username);
+
+        if (user == null) {
+            throw new UsernameNotFoundException("Could not find user");
+        }
+
+        return new MyUserDetails(user);
+    }
+
+}
